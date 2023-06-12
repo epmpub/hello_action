82372582f9c6008d4d143e583bd56b1a48c78dd5
andy hu <you@example.com>
2023-06-12 11:27:11 +0800

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("Hello Github!");
     println!("Hello Winget!");
-    println!("Test Github Action!");
-
+    println!("Hello Github Action!");
+    println!("Rust Project Github Action Release Template for Windows X86_64 Platform,No dependency VCRUNTIME140 Library" );
 }
+
